47411b1e720318938e206e1ddca6021d7b62f6f3
sireesha.bobbili <sireesha.bobbili@IQGLAPIQE317.iqgateway.com>
2025-07-26 13:40:25 +0530

    2nd commit

--- Lesson01GIT/src/training/iqgateway/HelloGIT.java
+++ Lesson01GIT/src/training/iqgateway/HelloGIT.java
@@ -10,5 +10,6 @@ public static void main(String[] args) {
 	System.out.println("This is a test for GIT 5");
 	System.out.println("This is a test for GIT 6");
 	System.out.println("This is a test for GIT 7");
+	System.out.println("This is a test for GIT 8");
 }
 }
