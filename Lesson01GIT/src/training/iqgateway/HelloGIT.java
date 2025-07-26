5fe06ee097dccb2e186fd7449c3281f9823d6f53
sireesha.bobbili <sireesha.bobbili@IQGLAPIQE317.iqgateway.com>
2025-07-26 13:10:01 +0530

    hello

---
+++ Lesson01GIT/src/training/iqgateway/HelloGIT.java
@@ -0,0 +1,14 @@
+package training.iqgateway;
+
+public class HelloGIT {
+public static void main(String[] args) {
+	System.out.println("Hello GIT");
+	System.out.println("This is a test for GIT");
+	System.out.println("This is a test for GIT 2");
+	System.out.println("This is a test for GIT 3");
+	System.out.println("This is a test for GIT 4");
+	System.out.println("This is a test for GIT 5");
+	System.out.println("This is a test for GIT 6");
+	System.out.println("This is a test for GIT 7");
+}
+}
